d90c85a189231c0b5ff6c540a0528aa32f6c5c10
nsato <nsato@deloitte.ca>
2023-01-20 01:40:08 -0500

    Add question and the solution for Day 5 part 1

--- Day 5/answer.js
+++ Day 5/answer.js
@@ -1,42 +1,47 @@
 const fs = require('fs');
 
-fs.readFile('./example.txt', "utf-8", (err, data)=>{
+fs.readFile('./input.txt', "utf-8", (err, data)=>{
   if(err) throw err;
 
   let craneInstruction = data.split('\n');
 
-  // First, find out how many stacks there are
-  // Loop through the array to find the 2nd element of the stack is number
-  let numOfStacks = 0;
+  // First, find out how many stacks there are and seprate stacks and instructions
+  let numOfStacks = (craneInstruction[0].length + 1) / 4;                       // Each stack occupy 4 spaces including white space and []. The last stack has one less white space, that's why + 1  
+  let numOfRows = -1;
   for(const stack of craneInstruction){
-    if(parseInt(stack[1]) > 0){
-      numOfStacks = parseInt(stack[stack.length - 2]);
+    numOfRows++;
+    if(stack[1].match(/\d/) > 0){
       break;
     }
   }
-  
-  let stacks = craneInstruction.splice(0, numOfStacks);          // Split craneInstruction into 2 parts: stacks and craneInstruction
-  craneInstruction = craneInstruction.splice(numOfStacks - 1);  
-  let numOfRows = stacks.length;                                 // Declare numOfRows and set it to stacks.length
+  let stacks = craneInstruction.splice(0, numOfRows);                         // Split craneInstruction into 2 parts: stacks and craneInstruction  
+  craneInstruction = craneInstruction.splice(2, craneInstruction.length - 1);  
 
-  let organizedStack = [];
-  for(let i = 0; i < numOfRows; i++){                            // Loop through rows
+  let organizedStack = Array.from({length: numOfStacks}, () => []); // Initialize organizedStack with numOfStacks number of empty arrays
+  for(let i = 0; i < stacks.length; i++){                           // Loop through rows
     let k = 1;
-    for(let j = 0; j < numOfStacks; j++){                        // Loop through stacks
-      if(stacks[i][k] != ' '){
-        organizedStack[j] = stacks[i][k];
+    for(let j = 0; j < numOfStacks; j++){                           // Loop through stacks
+      if(stacks[i][k].match(/[A-Z]/)){
+        organizedStack[j].push(stacks[i][k]);
       }
-      k += 4;                                                    // Every 4 indice has item(char)
+      k += 4;                                                       // Every 4 indices has item(char)
     }
   }
 
-  for(const element of craneInstruction){
-    let move = element[5];
-    let from = element[12];
-    let to = element[17];
-    console.log(`Move ${move} from ${from} to ${to}`);
-    for(let i = 0; i < organizedStack.length; i++){
-      console.log(organizedStack[i]);
+  for(const instruction of craneInstruction){
+    const match = instruction.match(/move (\d+) from (\d+) to (\d+)/);
+    const move = match[1];
+    const from = match[2] - 1;
+    const to = match[3] - 1;
+
+    console.log(`Move ${move} From ${from + 1} To ${to + 1}`);
+    let elementToMove = organizedStack[from].splice(0, move);
+    for(let i = 0; i < elementToMove.length; i++) {
+      organizedStack[to].splice(0, 0, elementToMove[i]);
+    }
+    
+    for(const element of organizedStack){
+      console.log(element);
     }
   }
 })
